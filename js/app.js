53f008bcac5edce0a351a6efb3321218d2bd66ed
Yulber Espinoza <yulbertime@gmail.com>
2024-08-04 23:20:15 -0500

    subir css nuevo

--- js/app.js
+++ js/app.js
@@ -1,97 +1,97 @@
-'use strict';
+"use strict";
 
 const img = [
-    'casacaAdidas',
-    'casacaAdidasBlanca',
-    'chalinaCuadros',
-    'chalinaMarron',
-    'chaquetaPloma',
-    'chompaEstrella',
-    'chompaRayas',
-    'poleraNegra',
-    'sacoBotones',
+  "casacaAdidas",
+  "casacaAdidasBlanca",
+  "chalinaCuadros",
+  "chalinaMarron",
+  "chaquetaPloma",
+  "chompaEstrella",
+  "chompaRayas",
+  "poleraNegra",
+  "sacoBotones",
 ];
 
 const state = {
-    totalProducts: [],
+  totalProducts: [],
 };
 
 class Products {
-    constructor(name, route) {
-        this.name = name;
-        this.route = route;
-        this.price = 200;
-    }
+  constructor(name, route) {
+    this.name = name;
+    this.route = route;
+    this.price = 200;
+  }
 
-    addToLocalStorage() {
-        const storedProducts = JSON.parse(localStorage.getItem('products')) || [];
-        storedProducts.push({
-            name: this.name,
-            route: this.route,
-            price: this.price,
-        });
-        localStorage.setItem('products', JSON.stringify(storedProducts));
-    }
+  addToLocalStorage() {
+    const storedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    storedProducts.push({
+      name: this.name,
+      route: this.route,
+      price: this.price,
+    });
+    localStorage.setItem("products", JSON.stringify(storedProducts));
+  }
 
-    capitalizeAndSpace(str) {
-        return str
-            .replace(/([A-Z])/g, ' $1')
-            .replace(/^./, (match) => match.toUpperCase());
-    }
+  capitalizeAndSpace(str) {
+    return str
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (match) => match.toUpperCase());
+  }
 
-    renderImg() {
-        const fill = document.getElementById('main');
-        const div = this.createProductElement();
-        fill.appendChild(div);
-    }
+  renderImg() {
+    const fill = document.getElementById("main");
+    const div = this.createProductElement();
+    fill.appendChild(div);
+  }
 
-    createProductElement() {
-        const div = document.createElement('div');
-        div.className = 'products';
+  createProductElement() {
+    const div = document.createElement("div");
+    div.className = "products";
 
-        const imgElement = document.createElement('img');
-        imgElement.src = this.route;
-        imgElement.alt = this.name;
-        imgElement.style.maxWidth = '50%';
+    const imgElement = document.createElement("img");
+    imgElement.src = this.route;
+    imgElement.alt = this.name;
+    imgElement.style.maxWidth = "40%";
 
-        const nameText = document.createElement('p');
-        nameText.textContent = this.capitalizeAndSpace(this.name);
+    const nameText = document.createElement("p");
+    nameText.textContent = this.capitalizeAndSpace(this.name);
 
-        const priceText = document.createElement('p');
-        priceText.textContent = `S/${this.price}`;
+    const priceText = document.createElement("p");
+    priceText.textContent = `S/${this.price}`;
 
-        const button = this.createBuyButton();
+    const button = this.createBuyButton();
 
-        div.appendChild(imgElement);
-        div.appendChild(nameText);
-        div.appendChild(priceText);
-        div.appendChild(button);
+    div.appendChild(imgElement);
+    div.appendChild(nameText);
+    div.appendChild(priceText);
+    div.appendChild(button);
 
-        return div;
-    }
+    return div;
+  }
 
-    createBuyButton() {
-        const button = document.createElement('button');
-        button.textContent = 'Agregar al carrito';
-        button.addEventListener('click', () => {
-            this.addToLocalStorage();
-        });
+  createBuyButton() {
+    const button = document.createElement("button");
+    button.textContent = "Agregar al carrito";
+    button.addEventListener("click", () => {
+      this.addToLocalStorage();
+    });
 
-        return button;
-    }
+    return button;
+  }
 }
 
 function objMaker() {
-    img.forEach(imageName => {
-        const product = new Products(imageName, `../img/${imageName}.png`);
-        state.totalProducts.push(product);
-    });
+  img.forEach((imageName) => {
+    const product = new Products(imageName, `../img/${imageName}.png`);
+    state.totalProducts.push(product);
+  });
 }
 
 function renderAllProducts() {
-    state.totalProducts.forEach(product => {
-        product.renderImg();
-    });
+  state.totalProducts.forEach((product) => {
+    product.renderImg();
+  });
 }
 objMaker();
 renderAllProducts();
